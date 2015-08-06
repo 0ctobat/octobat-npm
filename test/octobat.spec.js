7dc61a0019e5075a37180e42da7f556c32043ba5
Dimitri Jorge <jorge.dimitri@gmail.com>
2015-08-06 16:54:39 +0200

    First working version, highly inspirated by stripe's module

---
+++ test/octobat.spec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+var octobat = require('../lib/octobat')(process.env.OCTOBAT_TEST_API_KEY);
+
+describe('Octobat Module', function() {
+  describe('setTimeout', function() {
+    it('Should define a default equal to the node default', function() {
+      expect(octobat.getApiField('timeout')).to.equal(require('http').createServer().timeout);
+    });
+    it('Should allow me to set a custom timeout', function() {
+      octobat.setTimeout(900);
+      expect(octobat.getApiField('timeout')).to.equal(900);
+    });
+    it('Should allow me to set null, to reset to the default', function() {
+      octobat.setTimeout(null);
+      expect(octobat.getApiField('timeout')).to.equal(require('http').createServer().timeout);
+    });
+  });
+
+  describe('authentication', function() {
+    it('works when key is valid', function(done) {
+      octobat.customers.list({}, function(err, result) {
+        done(err);
+      });
+    });
+
+    it('returns an error when key is invalid', function(done) {
+      octobat.setApiKey('WRONG');
+
+      octobat.customers.list({}, function(err, result) {
+        expect(err).to.be.an('object');
+        done();
+      })
+    });
+  });
+});
---
+++ test/testUtils.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// NOTE: testUtils should be require'd before anything else in each spec file!
+
+require('mocha');
+// Ensure we are using the 'as promised' libs before any tests are run:
+require('chai').use(require('chai-as-promised'));
+
+var utils = module.exports = {
+
+  getUserOctobatKey: function() {
+    var key = process.env.OCTOBAT_TEST_API_KEY || throw "env's OCTOBAT_TEST_API_KEY not set";
+
+    return key;
+  },
+
+  getSpyableOctobat: function() {
+    // Provide a testable octobat instance
+    // That is, with mock-requests built in and hookable
+
+    var Octobat = require('../lib/octobat');
+    var octobatInstance = Octobat('fakeAuthToken');
+
+    octobatInstance.REQUESTS = [];
+
+    for (var i in octobatInstance) {
+      if (octobatInstance[i] instanceof Octobat.OctobatResource) {
+
+        // Override each _request method so we can make the params
+        // available to consuming tests (revealing requests made on
+        // REQUESTS and LAST_REQUEST):
+        octobatInstance[i]._request = function(method, url, data, auth, options, cb) {
+          var req = octobatInstance.LAST_REQUEST = {
+            method: method,
+            url: url,
+            data: data,
+            headers: options.headers || {},
+          };
+          if (auth) req.auth = auth;
+          octobatInstance.REQUESTS.push(req);
+          cb.call(this, null, {});
+        };
+
+      }
+    }
+
+    return octobatInstance;
+
+  },
+};
