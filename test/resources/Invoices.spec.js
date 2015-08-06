366c01c9c2db049b58e6cbdee79628ce4e4efbba
Dimitri Jorge <jorge.dimitri@gmail.com>
2015-08-06 18:25:02 +0200

    Add invoice resource

---
+++ test/resources/Invoices.spec.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var octobat = require('../testUtils').getSpyableOctobat();
+var expect = require('chai').expect;
+var Promise = require('bluebird');
+
+var TEST_AUTH_KEY = 'aGN0bIwXnHdw5645VABjPdSn8nWY7G11';
+
+describe('Invoices Resource', function() {
+  describe('create', function() {
+    var opts = {
+      livemode: false,
+      state: 'confirmed',
+      currency: 'usd',
+      customer_id: 'oc_cus_424424242',
+      evidence: {
+        field_1: 'meta1',
+        field_2: 'meta2',
+      },
+      invoice_items: [{}]
+    };
+
+    it('Sends the correct request', function() {
+      octobat.invoices.create(opts);
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: '/invoices',
+        headers: {},
+        data: opts
+      });
+    });
+
+    it('Sends the correct request [with specified auth]', function() {
+      octobat.invoices.create(opts, TEST_AUTH_KEY);
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: '/invoices',
+        headers: {},
+        data: opts,
+        auth: TEST_AUTH_KEY
+      });
+    });
+
+    it('Sends the correct request [with specified auth and no body]', function() {
+      octobat.invoices.create(TEST_AUTH_KEY);
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: '/invoices',
+        headers: {},
+        data: {},
+        auth: TEST_AUTH_KEY
+      });
+    });
+
+    it('Sends the correct request [with specified auth in options]', function() {
+      octobat.invoices.create(opts, { api_key: TEST_AUTH_KEY });
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: '/invoices',
+        headers: {},
+        data: opts,
+        auth: TEST_AUTH_KEY
+      });
+    });
+
+    it('Sends the correct request [with specified auth in options and no body]', function() {
+      octobat.invoices.create({ api_key: TEST_AUTH_KEY });
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: '/invoices',
+        headers: {},
+        data: {},
+        auth: TEST_AUTH_KEY
+      });
+    });
+  });
+});
