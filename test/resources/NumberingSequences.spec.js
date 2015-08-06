b78a6698d497c83c2aaccd9ccbdecca70b0e0ef4
Dimitri Jorge <jorge.dimitri@gmail.com>
2015-08-06 18:13:37 +0200

    Add numbering sequences

---
+++ test/resources/NumberingSequences.spec.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var octobat = require('../testUtils').getSpyableOctobat();
+var expect = require('chai').expect;
+
+var TEST_AUTH_KEY = 'aGN0bIwXnHdw5645VABjPdSn8nWY7G11';
+
+describe('Numbering Sequences Resource', function() {
+  describe('list', function() {
+    it('Sends the correct request', function() {
+      octobat.numberingSequences.list();
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'GET',
+        url: '/numbering_sequences',
+        headers: {},
+        data: {}
+      });
+    });
+
+    it('Sends the correct request [with specified auth]', function() {
+      octobat.numberingSequences.list(TEST_AUTH_KEY);
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'GET',
+        url: '/numbering_sequences',
+        headers: {},
+        data: {},
+        auth: TEST_AUTH_KEY
+      });
+    });
+
+    it('handles pagination', function() {
+      var opts = {
+        limit: 2,
+        starting_after: '1992-02-27'
+      };
+
+      octobat.numberingSequences.list(opts);
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'GET',
+        url: '/numbering_sequences',
+        headers: {},
+        data: opts
+      });
+    });
+  });
+});
