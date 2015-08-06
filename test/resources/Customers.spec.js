d77a00ee4e0518b55777bcdd27fb726d6c517b3e
Dimitri Jorge <jorge.dimitri@gmail.com>
2015-08-06 18:08:10 +0200

    Finish testing customer resource

--- test/resources/Customers.spec.js
+++ test/resources/Customers.spec.js
@@ -67,4 +67,80 @@ describe('Customers Resource', function() {
       });
     });
   });
+
+  describe('update', function() {
+    it('Sends the correct request', function() {
+      var opts = {
+        name: 'Bleh',
+        billing_address_line1: '42 moon street',
+        vat_number: '0123456789'
+      }
+
+      octobat.customers.update('oc_cu_1421878635hksc26e4de79', opts);
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: '/customers/oc_cu_1421878635hksc26e4de79',
+        headers: {},
+        data: opts
+      });
+    });
+  });
+
+  describe('create', function() {
+    var opts = { name: 'Some customer' };
+
+    it('Sends the correct request', function() {
+      octobat.customers.create(opts);
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: '/customers',
+        headers: {},
+        data: opts
+      });
+    });
+
+    it('Sends the correct request [with specified auth]', function() {
+      octobat.customers.create(opts, TEST_AUTH_KEY);
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: '/customers',
+        headers: {},
+        data: opts,
+        auth: TEST_AUTH_KEY
+      });
+    });
+
+    it('Sends the correct request [with specified auth and no body]', function() {
+      octobat.customers.create(TEST_AUTH_KEY);
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: '/customers',
+        headers: {},
+        data: {},
+        auth: TEST_AUTH_KEY
+      });
+    });
+
+    it('Sends the correct request [with specified auth in options]', function() {
+      octobat.customers.create(opts, { api_key: TEST_AUTH_KEY });
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: '/customers',
+        headers: {},
+        data: opts,
+        auth: TEST_AUTH_KEY
+      });
+    });
+
+    it('Sends the correct request [with specified auth in options and no body]', function() {
+      octobat.customers.create({ api_key: TEST_AUTH_KEY });
+      expect(octobat.LAST_REQUEST).to.deep.equal({
+        method: 'POST',
+        url: '/customers',
+        headers: {},
+        data: {},
+        auth: TEST_AUTH_KEY
+      });
+    });
+  });
 });
